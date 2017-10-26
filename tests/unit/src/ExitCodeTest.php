cdc0c5ee2e9a2052d0b9caaee00d1990ab6f41a5
Drasko Gomboc <drasko.gomboc@gmail.com>
2017-10-26 12:14:24 +0200

    Tests for ExitCode

---
+++ tests/unit/src/ExitCodeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use G4\CodeCoverage\ExitCode;
+use G4\CodeCoverage\Exceptions\InvalidExitCodeException;
+
+class ExitCodeTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testError()
+    {
+        $exitCode = ExitCode::error();
+        $this->assertInstanceOf(ExitCode::class, $exitCode);
+        $this->assertEquals(1, $exitCode->getValue());
+    }
+
+    public function testSuccess()
+    {
+        $exitCode = ExitCode::success();
+        $this->assertInstanceOf(ExitCode::class, $exitCode);
+        $this->assertEquals(0, $exitCode->getValue());
+    }
+
+    public function testValidValue()
+    {
+        $exitCode = new ExitCode(1);
+
+        $this->assertEquals(1, $exitCode->getValue());
+    }
+
+    public function testInvalidValue()
+    {
+        $this->expectException(InvalidExitCodeException::class);
+        new ExitCode(5);
+    }
+
+}
